25d9f96ab52a6e9b5a0b3886c5d03c224f469dfa
halitklyc <halitenes.kalayci@gmail.com>
2026-07-07 15:07:39 +0300

    add test2

--- test_login.py
+++ test_login.py
@@ -19,8 +19,24 @@ def test_invalid_login_shows_error():
     username_input.send_keys("deneme_selenium")
     password_input.send_keys("invalid_password")
     login_btn.click()
-    
     error_message = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "[data-test='error']")))
     # Assert -> Doğrulama aşaması (assert error message == şu)
     # testin tüm sonucu assertin yanına verdiğim koşula bağlı.
     assert error_message.text == "Epic sadface: Username and password do not match any user in this service"
+
+def test_valid_login():
+    # Arrange
+    driver = webdriver.Chrome()
+    driver.get("https://www.saucedemo.com/")
+    wait = WebDriverWait(driver, 10)
+    username_input = wait.until(EC.visibility_of_element_located((By.ID, "user-name")))
+    password_input = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "[data-test='password']")))
+    login_btn = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "[data-test='login-button']")))
+
+    # Act
+    username_input.send_keys("standard_user")
+    password_input.send_keys("secret_sauce")
+    login_btn.click()
+
+    # Assert
+    assert driver.current_url == "https://www.saucedemo.com/inventory.html"
