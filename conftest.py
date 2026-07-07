228d7eb8b918a9594b1168fd33c80c1a1fce86e4
halitklyc <halitenes.kalayci@gmail.com>
2026-07-07 15:17:54 +0300

    add conftest

--- conftest.py
+++ conftest.py
@@ -1,15 +1,25 @@
 from selenium import webdriver
 import pytest
 from selenium.webdriver.support.ui import WebDriverWait
-
+import random
 
 @pytest.fixture # her teste çalışmadan önce bu fonksiyon çalışacak ve driver objesini test fonksiyonuna gönderecek.
 def driver():
     driver = webdriver.Chrome()
     driver.get("https://www.saucedemo.com/")
-    yield driver
+    # yield => testi çalıştır
+    yield driver # return => fonksiyonu bitir ve döndür.
+    rnd = random.randint(1,1000)
+    driver.save_screenshot(filename=f"screenshot_{rnd}.png")
+    driver.quit()
+    # yield => döndür ama bitirme, devamı gelecek.
 
 @pytest.fixture
 def wait(driver):
     wait = WebDriverWait(driver, 20)
+    yield wait
+
+@pytest.fixture
+def wait_second(driver):
+    wait = WebDriverWait(driver, 10)
     yield wait
