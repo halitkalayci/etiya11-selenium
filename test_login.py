afe83f25aaaf62fd817e71f1d733b5e8cf92869b
halitklyc <halitenes.kalayci@gmail.com>
2026-07-07 15:33:19 +0300

    add parametrize

--- test_login.py
+++ test_login.py
@@ -3,20 +3,26 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
+import pytest
 
-def test_invalid_login_shows_error(driver,wait):
+
+# POM => Page Object Model
+
+# Dün yazdığımız testleri pytest'E geçirelim. conftest ve decoratorler kullanmaya özen gösterelim.
+
+@pytest.mark.parametrize("username,password,expected_error", [ 
+    ("abc","123","Epic sadface: Username and password do not match any user in this service") , 
+    ("","","Epic sadface: Username is required") , 
+    ("deneme","","Epic sadface: Password is required") ])
+def test_invalid_login_shows_error(driver,wait,username,password,expected_error):
     username_input = wait.until(EC.visibility_of_element_located((By.ID, "user-name")))
     password_input = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "[data-test='password']")))
     login_btn = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "[data-test='login-button']")))
-
-    # Act -> Aksiyon aşaması (logine tıkla, inputa yaz)
-    username_input.send_keys("deneme_selenium")
-    password_input.send_keys("invalid_password")
+    username_input.send_keys(username)
+    password_input.send_keys(password)
     login_btn.click()
     error_message = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "[data-test='error']")))
-    # Assert -> Doğrulama aşaması (assert error message == şu)
-    # testin tüm sonucu assertin yanına verdiğim koşula bağlı.
-    assert error_message.text == "Epic sadface: Username and password do not match any user in this service"
+    assert error_message.text == expected_error
 
 def test_valid_login(driver,wait):
     username_input = wait.until(EC.visibility_of_element_located((By.ID, "user-name")))
