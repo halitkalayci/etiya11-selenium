9d7acdf0df4f5a28a2b80817d887f032131b08ca
halitklyc <halitenes.kalayci@gmail.com>
2026-07-09 14:03:27 +0300

    add allure

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,12 +1,22 @@
 from selenium.webdriver.support import expected_conditions as EC
 from pages.login_page import LoginPage
+import allure
 
-
+@allure.title("Login sayfasında yanlış bilgilerle giriş.")
+@allure.description("Login sayfasında yanlış bilgilerle giriş yapıldığında hata mesajının görüntülendiğini doğrular.")
+@allure.severity(allure.severity_level.CRITICAL)
+@allure.issue("PROJ-12345","Login error bug") # jiraya bağla
+@allure.testcase("TC-061") # jiraya bağla
+@allure.link("https://youtube.com.tr", "Test Case") # custom link..
 def test_invalid_login_shows_error(driver,wait):
     login_page = LoginPage(driver,wait)
     login_page.login("abc","123")
     assert login_page.get_error_text() == "Epic sadface: Username and password do not match any user in this service"
 
+
+@allure.title("Geçerli giriş bilgileriyle giriş yapma.")
+@allure.description("Geçerli giriş bilgileriyle giriş yapıldığında kullanıcıların ana sayfasına yönlendirildiğini doğrular.")
+@allure.severity(allure.severity_level.BLOCKER)
 def test_valid_login(driver, wait):
     login_page = LoginPage(driver,wait)
     login_page.login("standard_user","secret_sauce")
