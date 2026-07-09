987469e6b27787063fd5c138a60e27f0a5a27809
halitklyc <halitenes.kalayci@gmail.com>
2026-07-09 14:32:29 +0300

    add attachment&step

--- tests/test_login.py
+++ tests/test_login.py
@@ -11,6 +11,7 @@ import allure
 def test_invalid_login_shows_error(driver,wait):
     login_page = LoginPage(driver,wait)
     login_page.login("abc","123")
+    allure.attach(driver.get_screenshot_as_png(), name="Ekran Görüntüsü", attachment_type=allure.attachment_type.PNG)
     assert login_page.get_error_text() == "Epic sadface: Username and password do not match any user in this service"
 
 
@@ -18,8 +19,14 @@ def test_invalid_login_shows_error(driver,wait):
 @allure.description("Geçerli giriş bilgileriyle giriş yapıldığında kullanıcıların ana sayfasına yönlendirildiğini doğrular.")
 @allure.severity(allure.severity_level.BLOCKER)
 def test_valid_login(driver, wait):
+    allure.step("Login sayfası açılır.")
     login_page = LoginPage(driver,wait)
+    allure.step("Geçerli kullanıcı adı ve şifre ile giriş yapılır.")
     login_page.login("standard_user","secret_sauce")
+    allure.step("Ekran görüntüsü alınır ve eklenir.")
+    allure.attach(driver.get_screenshot_as_png(), name="Ekran Görüntüsü", attachment_type=allure.attachment_type.PNG)
+    allure.attach(driver.page_source, name="Sayfa Kaynağı", attachment_type=allure.attachment_type.HTML)
+    allure.attach(driver.current_url, name="Mevcut URL", attachment_type=allure.attachment_type.TEXT)
     assert driver.current_url == "https://www.saucedemo.com/inventory.html"
 
 def test_username_required(driver,wait):
