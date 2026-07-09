8ffa4132fb2e6835f8c52d820bb28d0fa19d860e
halitklyc <halitenes.kalayci@gmail.com>
2026-07-09 14:41:59 +0300

    add allure

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,13 +4,17 @@ from selenium.webdriver.support.ui import WebDriverWait
 import datetime
 import os
 from pages.login_page import LoginPage
-
+import allure
 
 @pytest.fixture # her teste çalışmadan önce bu fonksiyon çalışacak ve driver objesini test fonksiyonuna gönderecek.
 def driver(request):
     driver = webdriver.Chrome()
     driver.get("https://www.saucedemo.com/")
     yield driver
+    with allure.step("Ekran görüntüsü alınır ve eklenir."):
+        allure.attach(driver.get_screenshot_as_png(), name="Ekran Görüntüsü", attachment_type=allure.attachment_type.PNG)
+        allure.attach(driver.page_source, name="Sayfa Kaynağı", attachment_type=allure.attachment_type.HTML)
+        allure.attach(driver.current_url, name="Mevcut URL", attachment_type=allure.attachment_type.TEXT)
     driver.quit()
 
 @pytest.fixture
@@ -31,6 +35,10 @@ def authed_driver():
     login_page = LoginPage(driver, wait)
     login_page.login("standard_user", "secret_sauce")
     yield driver
+    with allure.step("Ekran görüntüsü alınır ve eklenir."):
+        allure.attach(driver.get_screenshot_as_png(), name="Ekran Görüntüsü", attachment_type=allure.attachment_type.PNG)
+        allure.attach(driver.page_source, name="Sayfa Kaynağı", attachment_type=allure.attachment_type.HTML)
+        allure.attach(driver.current_url, name="Mevcut URL", attachment_type=allure.attachment_type.TEXT)
     driver.quit()
 
 @pytest.fixture
--- tests/test_login.py
+++ tests/test_login.py
@@ -11,7 +11,6 @@ import allure
 def test_invalid_login_shows_error(driver,wait):
     login_page = LoginPage(driver,wait)
     login_page.login("abc","123")
-    allure.attach(driver.get_screenshot_as_png(), name="Ekran Görüntüsü", attachment_type=allure.attachment_type.PNG)
     assert login_page.get_error_text() == "Epic sadface: Username and password do not match any user in this service"
 
 
@@ -19,14 +18,11 @@ def test_invalid_login_shows_error(driver,wait):
 @allure.description("Geçerli giriş bilgileriyle giriş yapıldığında kullanıcıların ana sayfasına yönlendirildiğini doğrular.")
 @allure.severity(allure.severity_level.BLOCKER)
 def test_valid_login(driver, wait):
-    allure.step("Login sayfası açılır.")
-    login_page = LoginPage(driver,wait)
-    allure.step("Geçerli kullanıcı adı ve şifre ile giriş yapılır.")
-    login_page.login("standard_user","secret_sauce")
-    allure.step("Ekran görüntüsü alınır ve eklenir.")
-    allure.attach(driver.get_screenshot_as_png(), name="Ekran Görüntüsü", attachment_type=allure.attachment_type.PNG)
-    allure.attach(driver.page_source, name="Sayfa Kaynağı", attachment_type=allure.attachment_type.HTML)
-    allure.attach(driver.current_url, name="Mevcut URL", attachment_type=allure.attachment_type.TEXT)
+    islem = 3120 / 0
+    with allure.step("Login sayfası açılır."):
+        login_page = LoginPage(driver,wait)
+    with allure.step("Geçerli kullanıcı adı ve şifre ile giriş yapılır."):
+        login_page.login("standard_user","secret_sauce")
     assert driver.current_url == "https://www.saucedemo.com/inventory.html"
 
 def test_username_required(driver,wait):
